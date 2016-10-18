49c592bfcfc28f7d8c4f2b64a9d7b11f35e2909b
zhaoHm <zhao@allinpalm.com>
2016-10-18 10:35:39 +0800

    fix file of PhysicalExamination

---
+++ whm_project/Class/PhysicalExamination/C/HmSelectInsuredController.h
@@ -0,0 +1,13 @@
+//
+//  HmSelectInsuredController.h
+//  whm_project
+//
+//  Created by zhaoHm on 16/10/18.
+//  Copyright © 2016年 chenJw. All rights reserved.
+//
+
+#import "JwBackBaseController.h"
+
+@interface HmSelectInsuredController : JwBackBaseController
+
+@end
