8beb08c14f35bf894e7d2571d6c1dc64a4606714
zhaoHm <zhao@allinpalm.com>
2016-10-26 10:01:15 +0800

    fix bug of ti jian

---
+++ whm_project/Class/Physical/Physical1/V/HmPhysicalMainPageCell.h
@@ -0,0 +1,13 @@
+//
+//  HmPhysicalMainPageCell.h
+//  whm_project
+//
+//  Created by zhaoHm on 16/10/24.
+//  Copyright © 2016年 chenJw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HmPhysicalMainPageCell : UITableViewCell
+
+@end
---
+++ whm_project/Class/Physical/Physical1/V/HmPhysicalMainPageCell.m
@@ -0,0 +1,24 @@
+//
+//  HmPhysicalMainPageCell.m
+//  whm_project
+//
+//  Created by zhaoHm on 16/10/24.
+//  Copyright © 2016年 chenJw. All rights reserved.
+//
+
+#import "HmPhysicalMainPageCell.h"
+
+@implementation HmPhysicalMainPageCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
