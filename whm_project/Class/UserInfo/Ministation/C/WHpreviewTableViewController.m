29a5fce22c215fb00b2a9d351f5578ea80676ce4
王义国 <837694117@qq.com>
2016-11-02 12:34:12 +0800

    预览

--- whm_project/Class/UserInfo/Ministation/C/WHpreviewTableViewController.m
+++ whm_project/Class/UserInfo/Ministation/C/WHpreviewTableViewController.m
@@ -38,6 +38,7 @@
 @property(nonatomic,strong)UILabel * numkehuLaber;
 @property(nonatomic,strong)UILabel * kehuLaber;
 @property(nonatomic,strong)UIView * headView;
+@property(nonatomic,strong)UIView * lineView2;
 
 
 @end
@@ -82,6 +83,7 @@
     self.headImage.layer.cornerRadius = kScreenWitdh *0.1;
     self.headImage.image = [UIImage imageNamed:@"test_head"];
     [self.headView addSubview:_headImage];
+    self.headView.backgroundColor = [UIColor redColor];
     //
     self.nameLaber = [[UILabel alloc]init];
     self.nameLaber.frame = CGRectMake(CGRectGetMaxX(self.headImage.frame)+5, CGRectGetMinY(self.headImage.frame), kScreenWitdh* 0.15, kScreenWitdh * 0.1);
@@ -111,7 +113,113 @@
     [self.headView addSubview:_companyLaber];
     //
     self.messBut = [UIButton buttonWithType:(UIButtonTypeSystem)];
-    //self.messBut.frame = CGRectMake(CGRectGetMaxX(<#CGRect rect#>), <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
+    self.messBut.frame = CGRectMake(CGRectGetMaxX(self.companyLaber.frame)+20, CGRectGetMidY(self.nameLaber.frame), 30, 30);
+    [self.messBut setBackgroundImage:[UIImage imageNamed:@"message"] forState:(UIControlStateNormal)];
+    self.messBut.layer.cornerRadius = 15.0;
+    [self.headView addSubview:_messBut];
+    [self.messBut addTarget:self action:@selector(messageAction:) forControlEvents:(UIControlEventTouchUpInside)];
+    //
+    self.telBut = [UIButton buttonWithType:(UIButtonTypeSystem)];
+    self.telBut.frame = CGRectMake(CGRectGetMaxX(self.messBut.frame)+5, CGRectGetMidY(self.nameLaber.frame), 30, 30);
+    [self.telBut setBackgroundImage:[UIImage imageNamed:@"tel"] forState:(UIControlStateNormal)];
+    self.telBut.layer.cornerRadius = 15.0;
+    [self.headView addSubview:_telBut];
+    [self.telBut addTarget:self action:@selector(telAction:) forControlEvents:(UIControlEventTouchUpInside)];
+   //
+    self.lineView1 = [[UIView alloc]init];
+    self.lineView1.frame = CGRectMake(0, CGRectGetMaxY(self.headImage.frame)+5, kScreenWitdh, 1);
+    self.lineView1.backgroundColor = [UIColor grayColor];
+    [self.headView addSubview:_lineView1];
+    //
+    
+    self.baojianImage = [[UIImageView alloc]init];
+    self.baojianImage.frame = CGRectMake(CGRectGetMinX(self.headImage.frame), CGRectGetMaxY(self.lineView1.frame)+5, 20, 20);
+    self.baojianImage.image = [UIImage imageNamed:@"baojian.png"];
+    [self.headView addSubview:_baojianImage];
+    //
+    self.baojianLaber = [[UILabel alloc]init];
+    self.baojianLaber.frame = CGRectMake(CGRectGetMaxX(self.baojianImage.frame)+2, CGRectGetMinY(self.baojianImage.frame), CGRectGetWidth(self.baojianImage.frame)*2.5, CGRectGetHeight(self.baojianImage.frame));
+    self.baojianLaber.text = @"保监认证";
+    self.baojianLaber.textColor = [UIColor whiteColor];
+    self.baojianLaber.font = [UIFont systemFontOfSize:11.0];
+    [self.headView addSubview:_baojianLaber];
+    //
+    self.renzhengImage = [[UIImageView alloc]init];
+    self.renzhengImage.frame = CGRectMake(CGRectGetMaxX(self.companyLaber.frame), CGRectGetMinY(self.baojianLaber.frame), CGRectGetWidth(self.baojianImage.frame), CGRectGetHeight(self.baojianImage.frame));
+    self.renzhengImage.image = [UIImage imageNamed:@"renzheng.png"];
+    [self.headView addSubview:_renzhengImage];
+    //
+    self.renzhengLaber = [[UILabel alloc]init];
+    self.renzhengLaber.frame = CGRectMake(CGRectGetMaxX(self.renzhengImage.frame)+2, CGRectGetMinY(self.renzhengImage.frame), CGRectGetWidth(self.baojianLaber.frame)*1.2, CGRectGetHeight(self.baojianLaber.frame));
+    self.renzhengLaber.textColor = [UIColor whiteColor];
+    self.renzhengLaber.text = @"快保家认证";
+    self.renzhengLaber.font = [UIFont systemFontOfSize:11.0];
+    [self.headView addSubview:_renzhengLaber];
+    self.lineView2 = [[UIView alloc]init];
+    self.lineView2.frame = CGRectMake(0, CGRectGetMaxY(self.baojianLaber.frame)+5, kScreenWitdh, 1);
+    self.lineView2.backgroundColor = [UIColor grayColor];
+    [self.headView addSubview:_lineView2];
+    
+    //产品类型咨询等设计
+    self.numchanpin = [[UILabel alloc]init];
+    self.numchanpin.frame = CGRectMake(CGRectGetMidX(self.baojianLaber.frame), CGRectGetMaxY(self.lineView2.frame)+5, kScreenWitdh*0.2, 30);
+    self.numchanpin.text = @"152";
+    self.numchanpin.textColor = [UIColor greenColor];
+    [self.headView addSubview:_numchanpin];
+    self.chanpinLaber = [[UILabel alloc]init];
+    self.chanpinLaber.frame = CGRectMake(CGRectGetMinX(self.numchanpin.frame), CGRectGetMaxY(self.numchanpin.frame)+3, 20, 20);
+    self.chanpinLaber.font = [UIFont systemFontOfSize:11.0];
+    self.chanpinLaber.text = @"产品";
+    self.chanpinLaber.textColor = [UIColor grayColor];
+    [self.headView addSubview:_chanpinLaber];
+    //
+    self.numzixunLaber = [[UILabel alloc]init];
+    self.numzixunLaber.frame = CGRectMake(CGRectGetMaxX(self.numchanpin.frame)+5, CGRectGetMinY(self.numchanpin.frame),CGRectGetWidth(self.numchanpin.frame), CGRectGetHeight(self.numchanpin.frame));
+    self.numzixunLaber.textColor = [UIColor blueColor];
+    self.numzixunLaber.text = @"60";
+    [self.headView addSubview:_numzixunLaber];
+    //
+    self.zixunLaber = [[UILabel alloc]init];
+    self.zixunLaber.frame = CGRectMake(CGRectGetMinX(self.numzixunLaber.frame), CGRectGetMinY(self.chanpinLaber.frame), CGRectGetWidth(self.chanpinLaber.frame), CGRectGetHeight(self.chanpinLaber.frame));
+    self.zixunLaber.textColor = [UIColor grayColor];
+    self.zixunLaber.font = [UIFont systemFontOfSize:11.0];
+    self.zixunLaber.text = @"咨询";
+    [self.headView addSubview:_zixunLaber];
+    
+    //
+    self.numbaodanLaber = [[UILabel alloc]init];
+    self.numbaodanLaber.frame = CGRectMake(CGRectGetMaxX(self.numzixunLaber.frame)+5, CGRectGetMinY(self.numzixunLaber.frame), CGRectGetWidth(self.numzixunLaber.frame), CGRectGetHeight(self.numzixunLaber.frame));
+    self.numbaodanLaber.textColor = [UIColor blueColor];
+    self.numbaodanLaber.text = @"250";
+    [self.headView addSubview:_numbaodanLaber];
+    
+    //
+    self.baodanLaber =[[UILabel alloc]init];
+    self.baodanLaber.frame = CGRectMake(CGRectGetMinX(self.numbaodanLaber.frame), CGRectGetMinY(self.zixunLaber.frame), CGRectGetWidth(self.zixunLaber.frame), CGRectGetHeight(self.zixunLaber.frame));
+    self.baodanLaber.textColor = [UIColor grayColor];
+    self.baodanLaber.font = [UIFont systemFontOfSize:11.0];
+    self.baodanLaber.text = @"保单体检";
+    [self.headView addSubview:_baodanLaber];
+    
+    //
+    self.numkehuLaber = [[UILabel alloc]init];
+    self.numkehuLaber.frame = CGRectMake(CGRectGetMaxX(self.numbaodanLaber.frame)+5, CGRectGetMinY(self.numbaodanLaber.frame), CGRectGetWidth(self.numbaodanLaber.frame), CGRectGetHeight(self.numbaodanLaber.frame));
+    self.numkehuLaber.textColor = [UIColor redColor];
+    self.numkehuLaber.text = @"300";
+    [self.headView addSubview:_numkehuLaber];
+    
+    //
+    self.kehuLaber = [[UILabel alloc]init];
+    self.kehuLaber.frame = CGRectMake(CGRectGetMinX(self.numkehuLaber.frame), CGRectGetMinY(self.baodanLaber.frame), CGRectGetWidth(self.baodanLaber.frame), CGRectGetHeight(self.baodanLaber.frame));
+    self.kehuLaber.textColor = [UIColor grayColor];
+    self.kehuLaber.font = [UIFont systemFontOfSize:11.0];
+    self.kehuLaber.text = @"服务客户";
+    [self.headView addSubview:_kehuLaber];
+    
+    
+    
+    
+    
     
     
     
@@ -125,6 +233,18 @@
     
 }
 
+//信息事件
+-(void)messageAction:(UIButton *)sender
+{
+    
+}
+
+//
+//电话事件
+-(void)telAction:(UIButton *)sender
+{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
