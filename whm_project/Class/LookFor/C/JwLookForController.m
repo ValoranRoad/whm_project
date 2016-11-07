ff7f8618652a3e9e5fdd57dac40430fdd84a0108
zhaoHm <zhao@allinpalm.com>
2016-11-07 10:17:28 +0800

    fix bug

--- whm_project/Class/LookFor/C/JwLookForController.m
+++ whm_project/Class/LookFor/C/JwLookForController.m
@@ -28,6 +28,7 @@
 @property (nonatomic, strong) UIButton *btnConditions;
 // index = 1
 @property (nonatomic, strong) UIButton *btnEssentialMSG;
+@property (nonatomic, strong) CALayer *segmentLineLayer;
 // 横向的UIScrollview
 @property (nonatomic, strong) UIScrollView *horScrollV;
 // 纵向的UIScrollview2 --- 筛选条件
@@ -74,6 +75,7 @@
 
     [self quartData];
       [self UI];
+    [self setupUI];
 }
 -(void)quartData
 {
@@ -153,7 +155,7 @@
     //
     // index = 0 btnConditions;
     self.btnConditions = [UIButton buttonWithType:UIButtonTypeCustom];
-    _btnConditions.frame = CGRectMake(0, CGRectGetMaxY(_searchView.frame), kScreenWitdh * 0.5, 44);
+    _btnConditions.frame = CGRectMake(0, CGRectGetMaxY(_searchView.frame), kScreenWitdh * 0.5, 54);
     [_btnConditions setTitle:@"筛选条件" forState:UIControlStateNormal];
     [_btnConditions setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [_btnConditions setBackgroundColor:[UIColor blueColor]];
@@ -173,6 +175,11 @@
     [_btnEssentialMSG setTitle:@"基本信息" forState:UIControlStateNormal];
     _btnEssentialMSG.titleLabel.font = [UIFont systemFontOfSize:15];
     [_bigScrollV addSubview:_btnEssentialMSG];
+    
+    self.segmentLineLayer = [CALayer layer];
+    _segmentLineLayer.frame = CGRectMake(CGRectGetMinX(_btnConditions.frame), CGRectGetMaxY(_btnConditions.frame) - 5, CGRectGetWidth(_btnConditions.frame), 5);
+    _segmentLineLayer.contents = (__bridge id _Nullable)[UIImage imageNamed:@"lookForInsured"].CGImage;
+    [self.view.layer addSublayer:_segmentLineLayer];
     
     // 横向的UIScrollview horScrollV;
     self.horScrollV = [[UIScrollView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_btnConditions.frame), kScreenWitdh, kScreenHeight - 64 - CGRectGetMaxY(_btnConditions.frame))];
@@ -381,191 +388,9 @@
     
 
 }
-#pragma mark -- Private
-// 点击方法
--(void)btnConditionsAction:(UIButton *)sender
+
+- (void)setupUI
 {
-    _horScrollV.contentOffset = CGPointMake(0, 0);
-    UILabel * lab = [[UILabel alloc]init];
-    lab.frame = CGRectMake(30, CGRectGetHeight([UIScreen mainScreen].bounds)*0.1, 60, 20);
-    lab.text = @"筛选条件";
-    lab.font = [UIFont systemFontOfSize:13.0];
-    lab.textColor =[UIColor grayColor];
-    [self.conditionScrollV addSubview:lab];
-    //
-    UIImageView * myImage1 = [[UIImageView alloc]init];
-    myImage1.frame = CGRectMake(CGRectGetMinX(lab.frame), CGRectGetMaxY(lab.frame)+20, 20, 20);
-    myImage1.image = [UIImage imageNamed:@"shoujin"];
-    [self.conditionScrollV addSubview:myImage1];
-    //
-    UILabel * Laber1 = [[UILabel alloc]init];
-    Laber1.frame = CGRectMake(CGRectGetMaxX(myImage1.frame)+10, CGRectGetMinY(myImage1.frame)-5, 60, 30);
-    Laber1.textColor = [UIColor grayColor];
-    Laber1.font = [UIFont systemFontOfSize:13.0];
-    Laber1.text = @"首年保费";
-    [self.conditionScrollV addSubview:Laber1];
-    self.myText1 = [[UITextField alloc]init];
-    self.myText1.frame = CGRectMake(CGRectGetWidth([UIScreen mainScreen].bounds)*0.45, CGRectGetMinY(Laber1.frame), CGRectGetWidth([UIScreen mainScreen].bounds)*0.5, CGRectGetHeight(Laber1.frame));
-    
-    self.myText1.borderStyle = UITextBorderStyleNone;
-    self.myText1.textColor = [UIColor grayColor];
-    self.myText1.font = [UIFont systemFontOfSize:13.0];
-    UIImageView * img1 = [[UIImageView alloc]init];
-    img1.frame = CGRectMake(0, 0, 20, 20);
-    self.myText1.rightView = img1;
-    self.myText1.rightViewMode = UITextFieldViewModeAlways;
-    self.myText1.keyboardType = UIKeyboardTypeNumberPad;
-    img1.image = [UIImage imageNamed:@"p_arrowleft"];
-    
-    [self.conditionScrollV addSubview:_myText1];
-    
-    UIView * lineView1 = [[UIView alloc]init];
-    lineView1.frame = CGRectMake(20, CGRectGetMaxY(_myText1.frame)+10, CGRectGetWidth([UIScreen mainScreen].bounds)-40, 1);
-    lineView1.backgroundColor = [UIColor grayColor];
-    [self.conditionScrollV addSubview:lineView1];
-    
-    //
-    
-    UIImageView * myImage2 = [[UIImageView alloc]init];
-    myImage2.frame = CGRectMake(CGRectGetMinX(lab.frame), CGRectGetMaxY(lineView1.frame)+10, 20, 20);
-    myImage2.image = [UIImage imageNamed:@"baojin"];
-    [self.conditionScrollV addSubview:myImage2];
-    //
-    UILabel * Laber2 = [[UILabel alloc]init];
-    Laber2.frame = CGRectMake(CGRectGetMaxX(myImage2.frame)+10, CGRectGetMinY(myImage2.frame)-5, 60, 30);
-    Laber2.textColor = [UIColor grayColor];
-    Laber2.font = [UIFont systemFontOfSize:13.0];
-    Laber2.text = @"保险金额";
-    [self.conditionScrollV addSubview:Laber2];
-    
-    self.myText2 = [[UITextField alloc]init];
-    self.myText2.frame = CGRectMake(CGRectGetWidth([UIScreen mainScreen].bounds)*0.45, CGRectGetMinY(Laber2.frame), CGRectGetWidth([UIScreen mainScreen].bounds)*0.5, CGRectGetHeight(Laber2.frame));
-    
-    self.myText2.borderStyle = UITextBorderStyleNone;
-    self.myText2.textColor = [UIColor grayColor];
-    self.myText2.font = [UIFont systemFontOfSize:13.0];
-    UIImageView * img2 = [[UIImageView alloc]init];
-    img2.frame = CGRectMake(0, 0, 20, 20);
-    self.myText2.rightView = img2;
-    self.myText2.rightViewMode = UITextFieldViewModeAlways;
-    self.myText2.keyboardType = UIKeyboardTypeNumberPad;
-    img2.image = [UIImage imageNamed:@"p_arrowleft"];
-    
-    [self.conditionScrollV addSubview:_myText2];
-    
-    UIView * lineView2 = [[UIView alloc]init];
-    lineView2.frame = CGRectMake(20, CGRectGetMaxY(_myText2.frame)+10, CGRectGetWidth([UIScreen mainScreen].bounds)-40, 1);
-    lineView2.backgroundColor = [UIColor grayColor];
-    [self.conditionScrollV addSubview:lineView2];
-    
-    //
-    UIImageView * myImage3 = [[UIImageView alloc]init];
-    myImage3.frame = CGRectMake(CGRectGetMinX(lab.frame), CGRectGetMaxY(lineView2.frame)+10, 20, 20);
-    myImage3.image = [UIImage imageNamed:@"qijian"];
-    [self.conditionScrollV addSubview:myImage3];
-    //    //
-    UILabel * Laber3 = [[UILabel alloc]init];
-    Laber3.frame = CGRectMake(CGRectGetMaxX(myImage3.frame)+10, CGRectGetMinY(myImage3.frame)-5, 60, 30);
-    Laber3.textColor = [UIColor grayColor];
-    Laber3.font = [UIFont systemFontOfSize:13.0];
-    Laber3.text = @"保险期间";
-    [self.conditionScrollV addSubview:Laber3];
-    
-    self.myText3 = [[UILabel alloc]init];
-    self.myText3.frame = CGRectMake(CGRectGetWidth([UIScreen mainScreen].bounds)*0.45, CGRectGetMinY(Laber3.frame), CGRectGetWidth([UIScreen mainScreen].bounds)*0.5, CGRectGetHeight(Laber3.frame));
-    //
-    //self.myText3.borderStyle = UITextBorderStyleNone;
-    self.myText3.textColor = [UIColor grayColor];
-    self.myText3.font = [UIFont systemFontOfSize:13.0];
-    UIImageView * img3 = [[UIImageView alloc]init];
-     img3.frame = CGRectMake(CGRectGetMaxX(self.myText3.frame)-20, CGRectGetMinY(self.myText3.frame), 20, 20);
-    //self.myText3.rightView = img3;
-    //self.myText3.rightViewMode = UITextFieldViewModeAlways;
-   // self.myText3.keyboardType = UIKeyboardTypeNumberPad;
-    img3.image = [UIImage imageNamed:@"p_arrowleft"];
-      [self.conditionScrollV addSubview:img3];
-    //
-    [self.conditionScrollV addSubview:_myText3];
-    //
-    
-    //给self.view添加一个手势监测；
-    UITapGestureRecognizer *tapGesture=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(onClickUILable:)];
-    self.myText3.userInteractionEnabled = YES;
-    //设置手指数 单指
-    
-    tapGesture.numberOfTouchesRequired = 1;
-    [self.myText3 addGestureRecognizer:tapGesture];
-    
-
-    
-    
-    
-    UIView * lineView3 = [[UIView alloc]init];
-    lineView3.frame = CGRectMake(20, CGRectGetMaxY(_myText3.frame)+10, CGRectGetWidth([UIScreen mainScreen].bounds)-40, 1);
-    lineView3.backgroundColor = [UIColor grayColor];
-    [self.conditionScrollV addSubview:lineView3];
-    //
-    //
-    UIImageView * myImage4 = [[UIImageView alloc]init];
-    myImage4.frame = CGRectMake(CGRectGetMinX(lab.frame), CGRectGetMaxY(lineView3.frame)+10, 20, 20);
-    myImage4.image = [UIImage imageNamed:@"jiaofei"];
-    [self.conditionScrollV addSubview:myImage4];
-    //    //
-    UILabel * Laber4 = [[UILabel alloc]init];
-    Laber4.frame = CGRectMake(CGRectGetMaxX(myImage4.frame)+10, CGRectGetMinY(myImage4.frame)-5, 60, 30);
-    Laber4.textColor = [UIColor grayColor];
-    Laber4.font = [UIFont systemFontOfSize:13.0];
-    Laber4.text = @"缴费期间";
-    [self.conditionScrollV addSubview:Laber4];
-    
-    self.myText4 = [[UILabel alloc]init];
-    self.myText4.frame = CGRectMake(CGRectGetWidth([UIScreen mainScreen].bounds)*0.45, CGRectGetMinY(Laber4.frame), CGRectGetWidth([UIScreen mainScreen].bounds)*0.5, CGRectGetHeight(Laber4.frame));
-    //
-   // self.myText4.borderStyle = UITextBorderStyleNone;
-    self.myText4.textColor = [UIColor grayColor];
-    self.myText4.font = [UIFont systemFontOfSize:13.0];
-    UIImageView * img4 = [[UIImageView alloc]init];
-    img4.frame = CGRectMake(CGRectGetMaxX(self.myText4.frame)-20, CGRectGetMinY(self.myText4.frame), 20, 20);
-//    self.myText4.rightView = img4;
-//    self.myText4.rightViewMode = UITextFieldViewModeAlways;
-//    self.myText4.keyboardType = UIKeyboardTypeNumberPad;
-    img4.image = [UIImage imageNamed:@"p_arrowleft"];
-    [self.conditionScrollV addSubview:img4];
-    //
-    [self.conditionScrollV addSubview:_myText4];
-    //
-    //给self.view添加一个手势监测；
-    UITapGestureRecognizer *tapGesture1 =[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(onClickUILable1:)];
-    self.myText4.userInteractionEnabled = YES;
-    //设置手指数 单指
-    
-    tapGesture.numberOfTouchesRequired=1;
-    
-    [self.myText4 addGestureRecognizer:tapGesture1];
-    
-    
-    UIView * lineView4 = [[UIView alloc]init];
-    lineView4.frame = CGRectMake(20, CGRectGetMaxY(_myText4.frame)+10, CGRectGetWidth([UIScreen mainScreen].bounds)-40, 1);
-    lineView4.backgroundColor = [UIColor grayColor];
-    [self.conditionScrollV addSubview:lineView4];
-    
-    //
-    self.serchBut = [UIButton buttonWithType:(UIButtonTypeSystem)];
-    self.serchBut.frame = CGRectMake(30, CGRectGetMaxY(lineView4.frame)+20, CGRectGetWidth([UIScreen mainScreen].bounds)-60, 50);
-    self.serchBut.backgroundColor = [UIColor colorWithHex:0xFF4545];
-    self.serchBut.layer.cornerRadius = 25;
-    [self.serchBut setTitle:@"高级搜索" forState:(UIControlStateNormal)];
-    [self.serchBut setTintColor:[UIColor whiteColor]];
-    [self.conditionScrollV addSubview:_serchBut];
-    
-
-
-}
-
--(void)btnEssentialMSGAction:(UIButton *)sender
-{
-    _horScrollV.contentOffset = CGPointMake(kScreenWitdh, 0);
-    
     self.basImg = [[UIImageView alloc]init];
     self.basImg.frame = CGRectMake(kScreenWitdh*0.4, CGRectGetHeight([UIScreen mainScreen].bounds)*0.1, 60, 60);
     self.basImg.image = [UIImage imageNamed:@"test_head"];
@@ -584,7 +409,7 @@
     Laber1.font = [UIFont systemFontOfSize:13.0];
     Laber1.text = @"性别";
     [self.essentialScrollV addSubview:Laber1];
-
+    
     self.basText1 = [[UITextField alloc]init];
     self.basText1.frame = CGRectMake(CGRectGetWidth([UIScreen mainScreen].bounds)*0.45, CGRectGetMinY(Laber1.frame), CGRectGetWidth([UIScreen mainScreen].bounds)*0.5, CGRectGetHeight(Laber1.frame));
     
@@ -640,7 +465,7 @@
     myImage3.frame = CGRectMake(CGRectGetMinX(myImage1.frame), CGRectGetMaxY(lineView2.frame)+10, 20, 20);
     myImage3.image = [UIImage imageNamed:@"test_year"];
     [self.essentialScrollV addSubview:myImage3];
-   
+    
     UILabel * Laber3 = [[UILabel alloc]init];
     Laber3.frame = CGRectMake(CGRectGetMaxX(myImage3.frame)+10, CGRectGetMinY(myImage3.frame)-5, 60, 30);
     Laber3.textColor = [UIColor grayColor];
@@ -666,8 +491,8 @@
     lineView3.frame = CGRectMake(20, CGRectGetMaxY(_basText3.frame)+10, CGRectGetWidth([UIScreen mainScreen].bounds)-40, 1);
     lineView3.backgroundColor = [UIColor grayColor];
     [self.essentialScrollV addSubview:lineView3];
-
-//
+    
+    //
     UIImageView * myImage4 = [[UIImageView alloc]init];
     myImage4.frame = CGRectMake(CGRectGetMinX(myImage1.frame), CGRectGetMaxY(lineView3.frame)+10, 20, 20);
     myImage4.image = [UIImage imageNamed:@"test_debts"];
@@ -678,7 +503,7 @@
     Laber4.font = [UIFont systemFontOfSize:13.0];
     Laber4.text = @"负债总额";
     [self.essentialScrollV addSubview:Laber4];
-
+    
     self.basText4 = [[UITextField alloc]init];
     self.basText4.frame = CGRectMake(CGRectGetWidth([UIScreen mainScreen].bounds)*0.45, CGRectGetMinY(Laber4.frame), CGRectGetWidth([UIScreen mainScreen].bounds)*0.5, CGRectGetHeight(Laber4.frame));
     self.basText4.borderStyle = UITextBorderStyleNone;
@@ -696,6 +521,32 @@
     lineView4.frame = CGRectMake(20, CGRectGetMaxY(_basText4.frame)+10, CGRectGetWidth([UIScreen mainScreen].bounds)-40, 1);
     lineView4.backgroundColor = [UIColor grayColor];
     [self.essentialScrollV addSubview:lineView4];
+}
+
+#pragma mark -- Private
+// 点击方法
+-(void)btnConditionsAction:(UIButton *)sender
+{
+    _horScrollV.contentOffset = CGPointMake(0, 0);
+    
+    [UIView animateWithDuration:1.0 animations:^{
+        CGRect rect_old = _segmentLineLayer.frame;
+        CGRect rect_new = CGRectMake(CGRectGetMinX(sender.frame), rect_old.origin.y, rect_old.size.width, rect_old.size.height);
+        _segmentLineLayer.frame = rect_new;
+    }];
+
+
+}
+
+-(void)btnEssentialMSGAction:(UIButton *)sender
+{
+    _horScrollV.contentOffset = CGPointMake(kScreenWitdh, 0);
+    
+    [UIView animateWithDuration:1.0 animations:^{
+        CGRect rect_old = _segmentLineLayer.frame;
+        CGRect rect_new = CGRectMake(CGRectGetMinX(sender.frame), rect_old.origin.y, rect_old.size.width, rect_old.size.height);
+        _segmentLineLayer.frame = rect_new;
+    }];
 
     
 }
@@ -745,10 +596,14 @@
     if ([scrollView isEqual:_horScrollV]) {
         if (scrollView.contentOffset.x < kScreenWitdh * 0.5) {
             // 滑动到左侧
+            CGRect rect_old = _segmentLineLayer.frame;
+            _segmentLineLayer.frame = CGRectMake(CGRectGetMinX(_btnConditions.frame), rect_old.origin.y, rect_old.size.width, rect_old.size.height);
         }
         else
         {
             // 滑动到右侧
+            CGRect rect_old = _segmentLineLayer.frame;
+            _segmentLineLayer.frame = CGRectMake(CGRectGetMinX(_btnEssentialMSG.frame), rect_old.origin.y, rect_old.size.width, rect_old.size.height);
         }
     }
 }
