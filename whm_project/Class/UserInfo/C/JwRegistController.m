b6e032ab419d3ec633be0d0598160efc32a11594
zhaoHm <zhao@allinpalm.com>
2016-11-22 13:30:25 +0800

    fix bug of xxxooo

--- whm_project/Class/UserInfo/C/JwRegistController.m
+++ whm_project/Class/UserInfo/C/JwRegistController.m
@@ -95,7 +95,7 @@
     dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
     dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
     dispatch_source_set_event_handler(_timer, ^{
-        if(timeout< 0){ //倒计时结束，关闭
+        if(timeout<= 0){ //倒计时结束，关闭
             dispatch_source_cancel(_timer);
             dispatch_async(dispatch_get_main_queue(), ^{
                 //设置界面的按钮显示 根据自己需求设置
@@ -112,14 +112,14 @@
                 [UIView setAnimationDuration:1];
                 //[self.rg.codeBut setTitle:[NSString stringWithFormat:@"%@秒后重新发送",strTime] forState:UIControlStateNormal];
                 self.timeLaber.text = [NSString stringWithFormat:@"%@s后重发",strTime];
-                NSLog(@"%@",self.timeLaber.text);
+                NSLog(@"---%@---%d",self.timeLaber.text,seconds);
                 [UIView commitAnimations];
                 self.CodeBut.userInteractionEnabled = NO;
             });
             timeout--;
         }
     });
-    
+    dispatch_resume(_timer);
     
     //
     if (self.telText.text.length != 0) {
